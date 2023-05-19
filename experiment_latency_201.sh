fbc2875bf8194e9c171e45747020e9084b0b07c9
Ye Qiao <qiaoy4@miamioh.edu>
2023-05-18 21:29:51 -0700

    update

--- experiment_latency_201.sh
+++ experiment_latency_201.sh
@@ -8,3 +8,6 @@ python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --f
 python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --flops_weight 0 --latency_weight 0.7
 python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --flops_weight 0 --latency_weight 0.8
 python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --flops_weight 0 --latency_weight 0.9
+
+
+python DARTS_evaluation/train_imagenet_dist.py --arch flops_07_darts_cifar10 --save flops_07_darts_cifar10 --gpu 0
--- experiment_latency_201_copy.sh
+++ experiment_latency_201_copy.sh
@@ -9,4 +9,7 @@ python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --f
 python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --flops_weight 0 --latency_weight 0.8
 python prune_launch.py --space nas-bench-201 --dataset cifar10  --max_node 4 --flops_weight 0 --latency_weight 0.9
 
-python prune_launch.py --space darts --dataset imagenet-1k  --max_node 4 --flops_weight 0 --latency_weight 0
+python prune_launch.py --space darts --dataset imagenet-1k  --max_node 4 --flops_weight 0.7 --latency_weight 0
+
+
+python DARTS_evaluation/train_imagenet_dist.py --arch flops_07_darts_cifar10 --save flops_07_darts_cifar10 --gpu 0
